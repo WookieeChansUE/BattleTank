1c6c1a45a2e65c4cce9e1de950793fec0299e5cf
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-03-25 00:33:26 -0500

    TankAimingComponent

--- Source/BattleTank/Public/Tank.h
+++ Source/BattleTank/Public/Tank.h
@@ -3,8 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h"//new includes above here
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -17,6 +18,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	//
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:	
 	// Sets default values for this pawn's properties
