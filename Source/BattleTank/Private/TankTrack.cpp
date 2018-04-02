dc446e69023a445fb39f0f94fbe746266ebcdb44
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-04-02 14:44:05 -0500

    throttle responds but doesnt move yet...but soooooooonnnnn

---
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+#include "Engine/World.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	//auto Time = GetWorld()->GetTimeSeconds();
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
+
+
+	//todo clamp actual throttle value to control top speed
+}
