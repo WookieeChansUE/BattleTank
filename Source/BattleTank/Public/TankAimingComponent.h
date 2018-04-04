bcb4a3a0efb5a4bcdc22ecb72d5373f9475af4a3
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-04-03 19:16:27 -0500

    Enums for targeting recticle

--- Source/BattleTank/Public/TankAimingComponent.h
+++ Source/BattleTank/Public/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+//Enum for aiming state
+UENUM()
+enum class EFiringState : uint8
+	{
+		Reloading,
+		Aiming,
+		Locked
+	};
+
 //Forward declaration
 class UTankTurret;
 class UTankBarrel; 
@@ -24,6 +33,8 @@ public:
 	void SetTurretReference(UTankTurret* TurretToSet);
 
 protected:
+	UPROPERTY(BlueprintReadOnly, Category = State)
+	EFiringState FiringState = EFiringState::Aiming;
 
 public:	
 	//todo add SetTurretReference
