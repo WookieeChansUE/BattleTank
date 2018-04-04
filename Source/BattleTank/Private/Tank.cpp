0c9e78c782f37077e19689d11dea95688828c56e
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-04-04 18:19:50 -0500

    Construct/BeginPlay BP logs

--- Source/BattleTank/Private/Tank.cpp
+++ Source/BattleTank/Private/Tank.cpp
@@ -16,6 +16,14 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s Dildo: Tank C++ Construct"), *TankName)
+}
+void ATank::BeginPlay()
+{
+	//NEEDED FOR BP BEGIN PLAY TO RUN!!!
+	Super::BeginPlay();
 }
 
 void ATank::AimAt(FVector HitLocation)
