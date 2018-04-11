d8ecfc546bf43bdf070a742c416ca6f7ae3ca4fd
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-04-11 17:31:27 -0500

    ensure is powerful

--- Source/BattleTank/Private/TankAimingComponent.cpp
+++ Source/BattleTank/Private/TankAimingComponent.cpp
@@ -27,7 +27,7 @@ void UTankAimingComponent::Initialise(UTankBarrel * BarrelToSet, UTankTurret* Tu
 
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
-	if (!Barrel) { return; };
+	if (!ensure(Barrel)) { return; };
 
 	FVector OutLaunchVelocity;
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
@@ -54,7 +54,7 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 
 void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 {
-	if(!Barrel || !Turret) { return; }
+	if(ensure(Barrel) || ensure(Turret)) { return; }
 	//work out diff between current barrel rotation and aimdirection
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
