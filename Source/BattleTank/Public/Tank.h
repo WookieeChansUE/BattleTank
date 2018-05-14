f2a7508669562869c55d8968d48ecc1a408f2cbd
WOOKIEE-DESKTOP\mriml <mrimler1983@gmail.com>
2018-05-14 17:11:55 -0500

    Health Bar

--- Source/BattleTank/Public/Tank.h
+++ Source/BattleTank/Public/Tank.h
@@ -15,11 +15,13 @@ public:
 	//Called by engine when actor damage dealt
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
 
+private:
 	// Sets default values for this pawn's properties
 	ATank();
 
-private:
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	int32 StartingHealth = 100;
 
